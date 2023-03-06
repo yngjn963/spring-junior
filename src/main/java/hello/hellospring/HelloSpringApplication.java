63ff51d44c70727083e8b1f3a4b824e5456259c2
yngjn963 <yngjn963@naver.com>
2023-03-06 22:31:18 +0900

    feat: 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
    프로젝트 생성, 라이브러리 살펴보기

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -8,6 +8,14 @@ public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+		/*
+		톰캣 웹서버 내장
+		 */
+
+		/*
+		라이브러리 살펴보기
+		메이븐이나 그래들 같은 빌드 도구는 의존 관계의 라이브러리르 관리한다.
+	 */
 	}
 
 }
