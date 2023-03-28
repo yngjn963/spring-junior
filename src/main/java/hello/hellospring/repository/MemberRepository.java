110ea0c5ca24886d669f495667cd0af8e66307ef
yngjn963 <yngjn963@naver.com>
2023-03-28 23:09:16 +0900

    feat: 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
    회원 도메인과 리포지토리 만들기

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -2,8 +2,13 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository {
 
     Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    // Optional이란? 제네릭 객체가 null일 때, 그대로 반환하지 않고 Optional로 감쌀 때 사용
 
 }
