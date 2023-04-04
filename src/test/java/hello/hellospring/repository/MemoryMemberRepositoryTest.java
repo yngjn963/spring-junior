ba0875357108d9e98a84698651846caf0f091c6c
yngjn963 <yngjn963@naver.com>
2023-04-04 22:48:42 +0900

    feat: 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
    회원 리포지토리 테스트 케이스 작성

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -9,12 +9,20 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach // 개별 테스트 종료 후 실행
+    public void afterEach() {
+        repository.clearStore(); // 개별 테스트 종료 후 저장소를 비워 다음 테스트를 준비한다(의존 관계에 있는 테스트끼리 영향을 받지 않도록 한다).
+    }
 
     @Test
     public void save() {
@@ -46,4 +54,29 @@ class MemoryMemberRepositoryTest {
 
         assertThat(result).isEqualTo(member1);
     }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+    // 테스트는 순서에 의존적이면 안된다.
+    // 테스트 순서는 소스 순서대로 동작하는 것이 아니기 때문에 다른 테스트에 영향을 받지 않도록 해야 한다.
+
+    /*
+    학습 시에는 클래스를 만들고 테스트하였지만,
+    반대로 어떠한 목적의 테스트(틀)를 만들고 이 후 구성한 클래스가 테스트에 맞는지 확인하는 방법이 있다.
+    이를 테스트 주도 개발(TDD)이라 한다.
+     */
 }
