3c23f87b714002a9290a59689b604cb558f917cb
yngjn963 <yngjn963@naver.com>
2023-04-05 23:27:20 +0900

    feat: 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
    회원 서비스 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,37 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     *
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원은 안된다고 가정하면
+        validateDuplicateMember(member); // ctrl + alt + shift + T = 메서드 추출
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> { // 이미 존재하면 -> 람다식
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+}
