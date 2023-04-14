088e00b5e33236d8583eeeff86a7f2210d06085f
yngjn963 <116531588+yngjn963@users.noreply.github.com>
2023-04-14 19:58:49 +0900

    feat: 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
    회원 서비스 테스트

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 public class MemberService {
     private final MemberRepository memberRepository;
+    
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
