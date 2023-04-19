bc874115739728774d0b9de30e3ee568592000bf
yngjn963 <yngjn963@naver.com>
2023-04-19 22:51:40 +0900

    feat: 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/web/MemberController.java
+++ src/main/java/hello/hellospring/web/MemberController.java
@@ -4,12 +4,12 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-@Controller
+@Controller // Spring 실행 시 Controller 객체를 들고 있는다 = 스프링 컨테이너에서 스프링 빈을 관리한다.
 public class MemberController {
-    private final MemberService memberService;
+    private final MemberService memberService; // 초기화 하지 않고 하나의 인스턴스를 공용으로 사용한다.
 
-    @Autowired
-    public MemberController(MemberService memberService) {
+    @Autowired // 스프링 컨테이너에서 MemberService를 연결한다.
+    public MemberController(MemberService memberService) { // 오류 발생: memberService가 스프링 빈으로 등록되어 있지 않다.
         this.memberService = memberService;
     }
 
