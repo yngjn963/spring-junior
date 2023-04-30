dbe4a8f3f9c206d7b401e0f9765fa7867e024fa8
yngjn963 <yngjn963@naver.com>
2023-04-30 23:19:11 +0900

    feat: 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
    회원 웹 기능 - 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // Spring 실행 시 Controller 객체를 들고 있는다 = 스프링 컨테이너에서 스프링 빈을 관리한다.
 public class MemberController {
@@ -34,6 +36,18 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+//        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // 회원가입이 끝나면 home 화면으로 redirect
     }
 
 }
--- src/main/java/hello/hellospring/controller/MemberForm.java
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -1,4 +1,13 @@
 package hello.hellospring.controller;
 
 public class MemberForm {
+    public String getName() {
+        return name; // input name 값과 매핑
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
 }
