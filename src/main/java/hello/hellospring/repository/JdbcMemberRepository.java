48cc140dce631d6cad757e72065ad25c085879de
yngjn963 <yngjn963@naver.com>
2023-05-16 23:15:31 +0900

    feat: 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
    순수 JDBC

--- src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -1,11 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,41 +15,154 @@ public class JdbcMemberRepository implements MemberRepository {
 
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
-    }
+    } // 스프링이 데이터 소스를 만들어 놓고 주입한다.
 
     @Override
     public Member save(Member member) {
         String sql = "insert into member(name) values(?)";
 
+//        Connection conn = dataSource.getConnection();
+//
+//        PreparedStatement pstmt = conn.prepareStatement(sql);
+//        pstmt.setString(1, member.getName());
+//
+//        pstmt.executeUpdate();
+
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-
         try {
             conn = getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setString(1, member.getName());
+
+            pstmt.executeUpdate();
+
+            rs = pstmt.getGeneratedKeys(); // Statement.RETURN_GENERATED_KEYS 옵션과 매칭되어 생성된 Key 반환
+
+            if (rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+
+            return member;
         } catch (Exception e) {
-
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs); // 리소스 반환(릴리즈)
         }
-
-        return null;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        String sql = "select * from member where id = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        String sql = "select * from member";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            List<Member> members = new ArrayList<>();
+            while(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                members.add(member);
+            }
+            return members;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        String sql = "select * from member where name = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
     }
 
     private Connection getConnection() {
         return DataSourceUtils.getConnection(dataSource);
     }
+
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
+    {
+        // 자원을 호출한 역순으로 반환
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
 }
